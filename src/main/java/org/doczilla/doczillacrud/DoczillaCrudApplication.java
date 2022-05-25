aecee020e4e24a0379afa83d96768bb02187366b
Kolesov Sergey <s.kolesov78@gmail.com>
2022-05-25 19:44:27 +0300

    Initial commit

---
+++ src/main/java/org/doczilla/doczillacrud/DoczillaCrudApplication.java
@@ -0,0 +1,13 @@
+package org.doczilla.doczillacrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoczillaCrudApplication {
+
+  public static void main(String[] args) {
+
+    SpringApplication.run(DoczillaCrudApplication.class, args);
+  }
+}
---
+++ src/test/java/org/doczilla/doczillacrud/DoczillaCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package org.doczilla.doczillacrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoczillaCrudApplicationTests {
+
+  @Test
+  void contextLoads() {
+
+  }
+}
